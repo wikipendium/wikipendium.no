ac95f03360fb20c876aef6c56fce961e753fa244
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-02-03 05:52:46 +0100

    Improved touch

--- wikipendium/wiki/static/js/autocompletrie.js
+++ wikipendium/wiki/static/js/autocompletrie.js
@@ -65,8 +65,14 @@
                     index = $(this).attr('data-idx');
                     ul.children().removeClass('active');
                     $(ul.children()[index]).addClass("active");
-                }).on('click touchstart',function(e){
+                }).on('touchstart',function(e){
                     index = $(this).attr('data-idx');
+                    ul.children().removeClass('active');
+                    $(ul.children()[index]).addClass("active");
+                }).on('click touchend',function(e){
+                    index = $(this).attr('data-idx');
+                    ul.children().removeClass('active');
+                    $(ul.children()[index]).addClass("active");
                     var url = $(ul.children()[index]).attr('data-url') || sb.val();
                     window.location = url;
                 });

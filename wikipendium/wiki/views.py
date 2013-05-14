ba5272f4fa0f779f2984392e675528753f35430a
Stian Jensen <me@stianj.com>
2013-05-14 23:57:06 +0200

    Now possible to create new articles without error message

--- wikipendium/wiki/views.py
+++ wikipendium/wiki/views.py
@@ -87,7 +87,7 @@ def edit(request, slug, lang='en'):
     try:
         articleContent = article.get_newest_content(lang)
     except:
-        articleContent = ArticleContent(article=article, lang=lang)
+        articleContent = None
 
     if request.method == 'POST':
         form = ArticleForm(request.POST)
@@ -98,11 +98,13 @@ def edit(request, slug, lang='en'):
             new_articleContent = form.save(commit=False)
             new_articleContent.article = article
             new_articleContent.edited_by = request.user
-            new_articleContent.lang = articleContent.lang
-            new_articleContent.parent = articleContent
+            if articleContent != None:
+                new_articleContent.lang = articleContent.lang
+                new_articleContent.parent = articleContent
             new_articleContent.save(lang)
-            articleContent.child = new_articleContent
-            articleContent.save(change_updated_time=False)
+            if articleContent != None:
+                articleContent.child = new_articleContent
+                articleContent.save(change_updated_time=False)
             return HttpResponseRedirect(new_articleContent.get_url())
     else:
         form = ArticleForm(instance=articleContent)

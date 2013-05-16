f3da19ca5b59da5839cdee6d0af2a3928e544f6c
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-05-16 12:02:39 +0200

    Enabled ÆØÅ for slugs

--- wikipendium/urls.py
+++ wikipendium/urls.py
@@ -1,8 +1,12 @@
+# -*- coding: utf8 -*-
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.simple import redirect_to
 
 admin.autodiscover()
+
+slug_regex = ur'[A-Za-z0-9æøåÆØÅ]+'
 
 urlpatterns = patterns('',
      url(r'^admin/', include(admin.site.urls)),
@@ -10,18 +14,18 @@ urlpatterns = patterns('',
      url(r'^$', 'wikipendium.wiki.views.home', name='home'),
      url(r'^new/$', 'wikipendium.wiki.views.new', name='newarticle'),
 
-     url(r'^(?P<x>[A-Za-z0-9]+[^/]*)/$', redirect_to, {'url': '/%(x)s'}),
-     url(r'^(?P<slug>[A-Za-z0-9]+)[^/]*$', 'wikipendium.wiki.views.article'),
-     url(r'^(?P<slug>[A-Za-z0-9]+)[^/]*/edit/$', 'wikipendium.wiki.views.edit'),
-     url(r'^(?P<slug>[A-Za-z0-9]+)[^/]*/history/$', 'wikipendium.wiki.views.history'),
-     url(r'^(?P<slug>[A-Za-z0-9]+)[^/]*/history/(?P<id>\d+)/$', view='wikipendium.wiki.views.history_single', kwargs={'lang':'en'}),
+     url(r'^(?P<x>' + slug_regex + '[^/]*)/$', redirect_to, {'url': '/%(x)s'}),
+     url(r'^(?P<slug>' + slug_regex + ')[^/]*$', 'wikipendium.wiki.views.article'),
+     url(r'^(?P<slug>' + slug_regex + ')[^/]*/edit/$', 'wikipendium.wiki.views.edit'),
+     url(r'^(?P<slug>' + slug_regex + ')[^/]*/history/$', 'wikipendium.wiki.views.history'),
+     url(r'^(?P<slug>' + slug_regex + ')[^/]*/history/(?P<id>\d+)/$', view='wikipendium.wiki.views.history_single', kwargs={'lang':'en'}),
 
-     url(r'^users/(?P<username>[A-Za-z0-9]+)/$', 'wikipendium.wiki.views.user', name='user'),
+     url(r'^users/(?P<username>' + slug_regex + ')/$', 'wikipendium.wiki.views.user', name='user'),
 
-     url(r'^(?P<slug>[A-Za-z0-9]+)[^/]*/(?P<lang>[a-z]+)/$', 'wikipendium.wiki.views.article'),
+     url(r'^(?P<slug>' + slug_regex + ')[^/]*/(?P<lang>[a-z]+)/$', 'wikipendium.wiki.views.article'),
 
-     url(r'^(?P<slug>[A-Za-z0-9]+)[^/]*/(?P<lang>[a-z]+)/edit/$', 'wikipendium.wiki.views.edit'),
-     url(r'^(?P<slug>[A-Za-z0-9]+)[^/]*/(?P<lang>[a-z]+)/history/$', 'wikipendium.wiki.views.history'),
-     url(r'^(?P<slug>[A-Za-z0-9]+)[^/]*/(?P<lang>[a-z]+)/history/(?P<id>\d+)/$', 'wikipendium.wiki.views.history_single'),
+     url(r'^(?P<slug>' + slug_regex + ')[^/]*/(?P<lang>[a-z]+)/edit/$', 'wikipendium.wiki.views.edit'),
+     url(r'^(?P<slug>' + slug_regex + ')[^/]*/(?P<lang>[a-z]+)/history/$', 'wikipendium.wiki.views.history'),
+     url(r'^(?P<slug>' + slug_regex + ')[^/]*/(?P<lang>[a-z]+)/history/(?P<id>\d+)/$', 'wikipendium.wiki.views.history_single'),
 
 )

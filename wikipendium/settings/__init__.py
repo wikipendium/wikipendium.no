4a9cd1acd5a69544b650c26eb606973c50dfc321
Stian Jensen <me@stianj.com>
2015-01-26 18:45:02 +0100

    Append FAKEAUTH as backend instead of replacing

--- wikipendium/settings/__init__.py
+++ wikipendium/settings/__init__.py
@@ -7,9 +7,14 @@ except ImportError, e:
     raise ImportError("Failed to import local settings")
 
 
+from django.conf.global_settings import (
+    AUTHENTICATION_BACKENDS as DEFAULT_AUTHENTICATION_BACKENDS
+)
+
 try:
     if DEBUG and ACTIVATE_FAKEAUTH == "YES, I WANT TO ENABLE PASSWORDLESS LOGIN" \
             + " FOR ALL USERS, INCLUDING SUPERUSERS":
-        AUTHENTICATION_BACKENDS = ('wikipendium.fakeauth.FakeAuthBackend',)
+        AUTHENTICATION_BACKENDS = DEFAULT_AUTHENTICATION_BACKENDS + \
+            ('wikipendium.fakeauth.FakeAuthBackend',)
 except NameError:
     pass

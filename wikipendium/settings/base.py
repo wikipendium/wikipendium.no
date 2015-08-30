118c7fab9445357e084a61e06e14261f68b265b7
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2015-08-31 00:52:43 +0200

    Move static files out of wiki app
    The static files are for all apps, so it makes more sense to keep it
    outside the wiki app.

--- wikipendium/settings/base.py
+++ wikipendium/settings/base.py
@@ -1,4 +1,5 @@
 # Django settings for wikipendium project.
+import os
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -56,6 +57,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(os.path.realpath(__file__)), '../static/'),
 )
 
 # List of finder classes that know how to find static files in

82edd93d02d7e47ac8975eaec106653fe4ac39b7
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2015-08-31 20:39:14 +0200

    Add search index rebuilding to jitishcron

--- wikipendium/jitishcron/tasks.py
+++ wikipendium/jitishcron/tasks.py
@@ -3,9 +3,17 @@ from wikipendium import settings
 import subprocess
 
 twenty_four_hours_in_seconds = 60 * 60 * 24
+fifteen_minutes_in_seconds = 60 * 15
 
 
 @task(min_interval_in_seconds=twenty_four_hours_in_seconds)
 def database_backup():
     if not settings.DEBUG:
         subprocess.Popen(['venv/bin/python', 'manage.py', 'backup-to-s3'])
+
+
+@task(min_interval_in_seconds=fifteen_minutes_in_seconds)
+def rebuild_search_index():
+    if not settings.DEBUG:
+        subprocess.Popen(['venv/bin/python', 'manage.py',
+                          'rebuild_index', '--noinput'])
